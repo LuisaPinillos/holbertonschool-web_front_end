c3faad46b35c567a9d2e07fe591f984d9b758caa
LuisaPinillos <luisapinillos20@gmail.com>
2022-09-02 17:55:23 -0500

    Closure and loops

---
+++ 0x0C-Javascript_advanced/3-classrooms.js
@@ -0,0 +1,18 @@
+// Closure and loops
+// This function returns the seat number
+
+function createClassRoom(numbersOfStudents) {
+    function studentSeat(seat) {
+        return function() {
+            return seat;
+        }
+    }
+
+    let students = [];
+    for (let i = 0; i < numbersOfStudents; i++) {
+        students.push(studentSeat(i + 1));
+    }
+    return students;
+}
+
+const classRoom = createClassRoom(10);
