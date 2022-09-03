dc7e702c5f1fcc9b4fbf3996338c414f20c0845d
LuisaPinillos <luisapinillos20@gmail.com>
2022-09-02 21:33:48 -0500

    divide and add functions

---
+++ 0x0C-Javascript_advanced/4-math.js
@@ -0,0 +1,20 @@
+// divideBy function -> divide two numbers
+// AddBy function -> second Number
+
+function divideBy(firstNumber) {
+    return function(secondNumber) {
+        return secondNumber / firstNumber;
+    }
+}
+
+function addBy (firstNumber) {
+    return function(secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+}
+
+const addBy100 = addBy(100);
+const addBy1000 = addBy(1000);
+const divideBy10 = divideBy(10);
+const divideBy100 = divideBy(100);
