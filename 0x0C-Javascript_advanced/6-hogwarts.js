de94b4897763d645c4d9334508ff91b88aa530b9
LuisaPinillos <luisapinillos20@gmail.com>
2022-09-06 17:32:32 -0500

    Private methods with closure

---
+++ 0x0C-Javascript_advanced/6-hogwarts.js
@@ -0,0 +1,41 @@
+// Private methods with closure
+
+function studentHogwarts() {
+    let privateScore = 0;
+    let name = null;
+
+    function changeScoreBy(points) {
+        privateScore += points;
+    }
+    return {
+        setName: function (newName) {
+            name = newName;
+        },
+        rewardStudent: function () {
+            changeScoreBy(1);
+        },
+        penalizeStudent: function () {
+            changeScoreBy(-1);
+        },
+        getScore: function () {
+            return `${name}: ${privateScore}`;
+        }
+    }
+}
+
+// Create otwo variables and those are an instance of studentHogwarts
+const harry = new studentHogwarts();
+harry.setName("Harry");
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+console.log(harry.getScore());
+
+const draco = new studentHogwarts();
+draco.setName("Draco");
+draco.rewardStudent();
+draco.penalizeStudent();
+draco.penalizeStudent();
+draco.penalizeStudent();
+console.log(draco.getScore());
