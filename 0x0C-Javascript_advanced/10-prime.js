7c29abed288c71eb8a0cfdef02b5d0922d64e73d
LuisaPinillos <luisapinillos20@gmail.com>
2022-09-06 23:02:00 -0500

    Execute the countprimenumbers function 100 times

---
+++ 0x0C-Javascript_advanced/10-prime.js
@@ -0,0 +1,28 @@
+// Function that count the prime numbers from 2 to 100
+// and It executed for 100 times, finally take timing execution.
+
+function countPrimeNumbers() {
+    let counter = 0;
+    for (let num = 2; num <= 100; num++) {
+        let isprime = true;
+
+        for (let div = 2; div < num; div++) {
+            if (num % div === 0) {
+                isprime = false;
+            }
+        }
+        if (isprime == true) {
+            counter += 1;
+        }
+    }
+    return counter;
+}
+
+let Tinit = performance.now();
+//execute the function 100 times.
+for (let i = 0; i < 100; i++) {
+    countPrimeNumbers();
+}
+let Tfinish = performance.now();
+
+console.log(`Execution time of printing countPrimeNumbers was ${Tfinish - Tinit} milliseconds.`)
